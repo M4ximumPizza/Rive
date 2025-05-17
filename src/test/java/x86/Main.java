8888d8f63aa09f5b28bf582c2b107cbed86af03c
M4ximumPizza <109777745+M4ximumPizza@users.noreply.github.com>
2025-05-16 18:59:13 -0700

    Removed a method that is not needed.

--- src/test/java/x86/Main.java
+++ src/test/java/x86/Main.java
@@ -3,10 +3,6 @@ package x86;
 import java.io.IOException;
 
 public class Main {
-    private static void runall() {
-        MemoryTest.run();
-    }
-
     public static void main(String[] args) throws IOException {
         System.out.println("Running ALL TEST");
         CodeTest.run();
