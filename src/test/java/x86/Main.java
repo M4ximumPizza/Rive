e615aac43d952d74cd8fd80c8c125f312f4d77f1
M4ximumpizza <LWabernathy@outlook.com>
2025-08-10 23:35:22 -0700

    Significant refactoring

--- src/test/java/x86/Main.java
+++ src/test/java/x86/Main.java
@@ -6,8 +6,5 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Running ALL TEST");
         CodeTest.run();
-        MemoryTest.run();
-        RegisterLexerTest.run();
-        RegisterTest.run();
     }
 }
